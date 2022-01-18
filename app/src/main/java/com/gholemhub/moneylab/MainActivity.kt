58a18cea8357f455dedffe4f0af8483623bf22f2
GholemHub <segeda.olexandr@gmail.com>
2022-01-18 13:15:50 +0100

    NavigationBar

--- app/src/main/java/com/gholemhub/moneylab/MainActivity.kt
+++ app/src/main/java/com/gholemhub/moneylab/MainActivity.kt
@@ -2,19 +2,43 @@ package com.gholemhub.moneylab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
 import com.gholemhub.moneylab.databinding.ActivityMainBinding
+import com.gholemhub.moneylab.viewmodels.MainActivityViewModel
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), LifecycleOwner{
+
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
 
+
+
         setContentView(view)
 
-        binding.text.setText("KOKOKO")
+        binding.bnv?.background = null
+        binding.bnv?.menu?.getItem(1)?.isEnabled = false
+
+
+
+        var m = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        var j = 0
+
+
+        binding.text.setOnClickListener {
+            m.i += 1
+            j += 1
+            binding.text.setText("${m.i} :: $j")
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/gholemhub/moneylab/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.gholemhub.moneylab.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+    var i = 0;
+}
