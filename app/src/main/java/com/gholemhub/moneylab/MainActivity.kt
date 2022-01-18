54078414d5b1644b3587a5a0a8dd9759c6369d2b
GholemHub <segeda.olexandr@gmail.com>
2022-01-18 16:17:15 +0100

    [G] Fragrmnts

--- app/src/main/java/com/gholemhub/moneylab/MainActivity.kt
+++ app/src/main/java/com/gholemhub/moneylab/MainActivity.kt
@@ -4,11 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
-<<<<<<< HEAD
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-=======
->>>>>>> 58a18cea8357f455dedffe4f0af8483623bf22f2
 import com.gholemhub.moneylab.databinding.ActivityMainBinding
 import com.gholemhub.moneylab.viewmodels.MainActivityViewModel
 
@@ -28,13 +25,8 @@ class MainActivity : AppCompatActivity(), LifecycleOwner{
 
         setContentView(view)
 
-<<<<<<< HEAD
 
         MenuListener()
-=======
-        binding.bnv?.background = null
-        binding.bnv?.menu?.getItem(1)?.isEnabled = false
->>>>>>> 58a18cea8357f455dedffe4f0af8483623bf22f2
 
 
 
@@ -42,18 +34,13 @@ class MainActivity : AppCompatActivity(), LifecycleOwner{
 
         var j = 0
 
-<<<<<<< HEAD
 /*
-=======
-
->>>>>>> 58a18cea8357f455dedffe4f0af8483623bf22f2
         binding.text.setOnClickListener {
             m.i += 1
             j += 1
             binding.text.setText("${m.i} :: $j")
         }
 
-<<<<<<< HEAD
 */
     }
 
@@ -68,8 +55,6 @@ class MainActivity : AppCompatActivity(), LifecycleOwner{
         var navigationContoller = findNavController(R.id.containerFragment)
 
         binding.bnv?.setupWithNavController(navigationContoller)
-=======
->>>>>>> 58a18cea8357f455dedffe4f0af8483623bf22f2
 
     }
 }
