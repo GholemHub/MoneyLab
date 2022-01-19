713e37043496bacecd8c96a5c241d4bbb3a010d9
GholemHub <segeda.olexandr@gmail.com>
2022-01-19 14:49:29 +0100

    [G] calculator's btns and functions

--- app/src/main/java/com/gholemhub/moneylab/AddActivity.kt
+++ app/src/main/java/com/gholemhub/moneylab/AddActivity.kt
@@ -1,8 +1,11 @@
 package com.gholemhub.moneylab
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.toSpannable
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -22,7 +25,70 @@ class AddActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
-
+        //Disable keyboard on editText
+        binding.inputText.showSoftInputOnFocus = false
 
     }
+
+private fun updateText(newStr: String){
+    var oldStr = binding.inputText.text.toString()
+    var coursorPos = binding.inputText.selectionStart
+    var leftStr = oldStr.substring(0, coursorPos)
+    var rightStr = oldStr.substring(coursorPos)
+    binding.inputText.setText(String.format("%s%s%s", leftStr, newStr, rightStr))
+    binding.inputText.setSelection(coursorPos+1)
+}
+
+    fun btnListener_zero(View: View){
+        updateText("0")
+    }
+    fun btnListener_zero_zero(View: View){
+        updateText("00")
+    }
+    fun btnListener_one(View: View){
+        updateText("1")
+    }
+    fun btnListener_two(View: View){
+        updateText("2")
+    }
+    fun btnListener_three(View: View){
+        updateText("3")
+    }
+    fun btnListener_four(View: View){
+        updateText("4")
+    }
+    fun btnListener_five(View: View){
+        updateText("5")
+    }
+    fun btnListener_six(View: View){
+        updateText("6")
+    }
+    fun btnListener_seven(View: View){
+        updateText("7")
+    }
+    fun btnListener_eight(View: View){
+        updateText("8")
+    }
+    fun btnListener_nine(View: View){
+        updateText("9")
+    }
+    fun btnListener_clear(View: View){
+        binding.inputText.setText("")
+    }
+
+    fun btnListener_backspace(View: View){
+
+        var cursorePos = binding.inputText.selectionStart
+        var textLen = binding.inputText.text.length
+
+        if(cursorePos != 0 && textLen != 0){
+            var selection = binding.inputText.text.toSpannable()
+
+            selection.replaceRange(cursorePos-1, cursorePos, "")
+            binding.inputText.setText(selection)
+            binding.inputText.setSelection(cursorePos -1)
+
+        }
+    }
+
 }
